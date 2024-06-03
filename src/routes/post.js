900167e069c1bd88db6896bdc8c70fff55743d6a
Kaliztro <g.lopes1999@hotmail.com>
2024-06-03 01:39:16 -0300

    Melhorias

--- src/routes/post.js
+++ src/routes/post.js
@@ -4,23 +4,26 @@ const Caderno = require('../models/db_models');
 const dataAtual = require('../functions/data');
 
 router.post("/", async (req, res) => {
-    const caderno = new Caderno({
-        nome: req.body.nome,
-        valor: req.body.valor,
-        contato: req.body.contato,
-        data: req.body.data,
-    })
+    try {
+        const { nome, valor, contato, data } = req.body;
 
-    caderno.data === undefined ? caderno.data = dataAtual() : caderno.data
+        if (!nome) return res.status(400).send("Campo 'Nome' deve ser preenchido");
+        if (!contato) return res.status(400).send("Campo 'Contato' deve ser preenchido");
+        if (!valor) return res.status(400).send("Campo 'Valor' deve ser um número");
 
-    if (caderno.valor === undefined) {
-        return  res.status(400).send("Campo 'Valor' deve ser um número");
-    } else {
-        
-        // console.log(caderno.contato)
-        // return res.send(caderno)
-        await caderno.save()
-        return res.send(`Cliente registrado com sucesso! \n\n ${caderno.nome}\n ${caderno.valor} \n ${caderno.data}`)
+        const caderno = new Caderno({
+            nome,
+            valor,
+            contato,
+            data: data || dataAtual() 
+        });
+
+        await caderno.save();
+        return res.send(`Cliente registrado com sucesso! \n\n Nome: ${caderno.nome}\n Valor: ${caderno.valor} \n Data: ${caderno.data}`);
+
+    } catch (error) {
+        console.error('Erro ao registrar cliente:', error);
+        return res.status(500).send("Erro ao registrar cliente");
     }
 });
 
